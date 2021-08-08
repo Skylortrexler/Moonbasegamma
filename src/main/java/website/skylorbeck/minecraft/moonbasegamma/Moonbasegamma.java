ba9c7ffb77799a2ab51cdb06eccd51afc9fbdac8
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-08-08 14:20:17 -0700

    diorite and oxygen textures

--- src/main/java/website/skylorbeck/minecraft/moonbasegamma/Moonbasegamma.java
+++ src/main/java/website/skylorbeck/minecraft/moonbasegamma/Moonbasegamma.java
@@ -8,3 +8,19 @@ public class Moonbasegamma implements ModInitializer {
 
     }
 }
+//todo
+// invisible fluid
+// diorite reskin
+// iron armor reskin
+// horse armor reskin
+// creeper reskin
+// skeleton reskin
+// zombie reskin
+// enderman reskin
+// pigman reskin
+// crossbow reskin into raygun
+// skeletal horsemen reskin - skeleton horse, skeleton rider
+// netherrack, bastion blocks reskinned
+// end dragon reskin
+// endstone reskin
+// end crystal reskin
